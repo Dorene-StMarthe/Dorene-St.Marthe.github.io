d074569c0cb520c58d74391e4ad6af425945e0a4
Dorene07 <dstmarth@gmail.com>
2022-07-06 19:34:43 -0400

    added in alerts for initial user response

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+
+alert("Hi! Congratulations on adopting your tamagotchi pet!")
+alert("Here at Gotchi-Pets, we strive to match each pet with their perfect pet-mate!")
+
+let person = prompt("What is your name?")
+
+alert(person + ", your new pet-mate is so excited to play and spend time with you!")
+alert("Before we let you go, there are a few things you should know to keep your pet happy")
+alert("Your pet loves to eat, sleep and play. Keep each of their levels up by interacting with them to prevent them from dieing")
