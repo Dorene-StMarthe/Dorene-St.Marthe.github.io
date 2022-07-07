32f8dd799bb8b60d70f348d6bb9581ed1bfda32e
Dorene07 <dstmarth@gmail.com>
2022-07-06 23:45:23 -0400

    added on-click function

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,14 @@
 
+const enter = document.querySelector('#enter');
+enter.addEventListener('click', () => {
 
-/*alert("Hi! Congratulations on adopting your tamagotchi pet!")
-alert("Here at Gotchi-Pets, we strive to match each pet with their perfect pet-mate!")
+alert("Hi! Congratulations on adopting your tamagotchi pet!");
+alert("Here at Gotchi-Pets, we strive to match each pet with their perfect pet-mate!");
 
-let person = prompt("What is your name?")
-let text;
-if (person == null || person == "") {
-  prompt = "Please enter your name"};
+let person = prompt("What is your name?");
 
-alert("Hello, " + person + " your new pet-mate is so excited to play and spend time with you!")
-alert("Before we let you go, there are a few things you should know to keep your pet happy.")
-alert("Your pet loves to eat, sleep and play all day. Keep each of their levels up by interacting with them to prevent them from dying")*/
+alert("Hello, " + person + " your new pet-mate is so excited to play and spend time with you!");
+alert("Before we let you go, there are a few things you should know to keep your pet happy.");
+alert("Your pet loves to eat, sleep and play all day. Keep each of their levels up by interacting with them to prevent them from dying");
 
+});
