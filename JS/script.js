a17f7fb2b5bc595ab62da6698bccc48a3a584621
Dorene07 <dstmarth@gmail.com>
2022-07-06 21:35:58 -0400

    added css background

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,14 @@
 
 
-alert("Hi! Congratulations on adopting your tamagotchi pet!")
+/*alert("Hi! Congratulations on adopting your tamagotchi pet!")
 alert("Here at Gotchi-Pets, we strive to match each pet with their perfect pet-mate!")
 
 let person = prompt("What is your name?")
+let text;
+if (person == null || person == "") {
+  prompt = "Please enter your name"};
 
-alert(person + ", your new pet-mate is so excited to play and spend time with you!")
-alert("Before we let you go, there are a few things you should know to keep your pet happy")
-alert("Your pet loves to eat, sleep and play. Keep each of their levels up by interacting with them to prevent them from dieing")
+alert("Hello, " + person + " your new pet-mate is so excited to play and spend time with you!")
+alert("Before we let you go, there are a few things you should know to keep your pet happy.")
+alert("Your pet loves to eat, sleep and play all day. Keep each of their levels up by interacting with them to prevent them from dying")*/
+
