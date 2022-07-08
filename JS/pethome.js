5cb9e0122c7faaadb233b98ee067ffe1f1496aa1
Dorene07 <dstmarth@gmail.com>
2022-07-08 14:46:42 -0400

    adjusted timer

--- JS/pethome.js
+++ JS/pethome.js
@@ -1,30 +1,59 @@
-function game() {} [
-    incPlay,
-    incFeed,
-    lights,
-]
 
-//playtime toggle
-setInterval(incPlay, 2000);
+
+
+
+    
+    
+// button toggle to decrease time
+let btnIncPlay = document.querySelector('#incPlay');
+let input = document.querySelector('.Play');
+
+btnIncPlay.addEventListener('click', () => {
+    input.value = parseInt(input.value) - 1;
+});
+
+
+let btnIncFeed = document.querySelector('#incFeed');
+let input1 = document.querySelector('.Feed');
+
+btnIncFeed.addEventListener('click', () => {
+    input1.value = parseInt(input1.value) - 1;
+});
+
+
+let btnLightsToggle = document.querySelector('#lightsToggle');
+let input2 = document.querySelector('.Lights');
+
+btnLightsToggle.addEventListener('click', () => {
+    input2.value = parseInt(input2.value) - 1;
+});
+
+
+
+
+
+
+
+
+/*setInterval(incPlay, 3000);
     function incPlay() {
-    let element = document.getElementById('letsPlay');
+    let element = document.getElementById('incPlay');
     let value = element.innerHTML;
     ++value;
     console.log(value)
-    document.getElementById('letsPlay').innerHTML = value;
-}
+    document.getElementById('inPlay').innerHTML = value;}
 
 //feeding toggle
-setInterval(incFeed, 2000);
+setInterval(incFeed, 3000);
 function incFeed() {
     let element = document.getElementById('feedMe');
     let value = element.innerHTML;
     ++value;
-    console.log(value)
+    console.log(value);
     document.getElementById('feedMe').innerHTML = value;
 }
 //lights toggle
-setInterval(lights, 2000);
+setInterval(lights, 3000);
 function lights() {
     let element = document.getElementById('lightsToggle');
     let value = element.innerHTML;
@@ -38,9 +67,20 @@ function lightsOnOff() {
 }
 
 
+//conditions
 
 //Array of Functions
 
 //if (this.incPlay[value] >= 10) {
   //  alert( "Your pet has died and gone to pet heaven!")
 //this.value -=10;
+
+
+setInterval(incPlay, 3000);
+    function incPlay() {
+    let element = document.getElementById('letsPlay');
+    let value = element.innerHTML;
+    ++value;
+    console.log(value)
+    document.getElementById('letsPlay').innerHTML = value;
+    }*/
