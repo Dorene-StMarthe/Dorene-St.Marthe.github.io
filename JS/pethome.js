8a6d4d1486dca9064e040c5f76578633d19c40df
Dorene07 <dstmarth@gmail.com>
2022-07-08 02:59:41 -0400

    configured

--- JS/pethome.js
+++ JS/pethome.js
@@ -1,13 +1,21 @@
+function game() {} [
+    incPlay,
+    incFeed,
+    lights,
+]
 
 //playtime toggle
-function incPlay() {
+setInterval(incPlay, 2000);
+    function incPlay() {
     let element = document.getElementById('letsPlay');
     let value = element.innerHTML;
     ++value;
     console.log(value)
     document.getElementById('letsPlay').innerHTML = value;
 }
+
 //feeding toggle
+setInterval(incFeed, 2000);
 function incFeed() {
     let element = document.getElementById('feedMe');
     let value = element.innerHTML;
@@ -16,6 +24,7 @@ function incFeed() {
     document.getElementById('feedMe').innerHTML = value;
 }
 //lights toggle
+setInterval(lights, 2000);
 function lights() {
     let element = document.getElementById('lightsToggle');
     let value = element.innerHTML;
@@ -28,14 +37,10 @@ function lightsOnOff() {
     element.classList.toggle("dark-mode");
 }
 
-//Array of Functions
-const game = [
-    incPlay,
-    incFeed,
-    lights,
-    lightsOnOff,
-]
 
-if (i >= 10 ) {
-    alert("Your pet has died and gone to pet heaven!");
-}
+
+//Array of Functions
+
+//if (this.incPlay[value] >= 10) {
+  //  alert( "Your pet has died and gone to pet heaven!")
+//this.value -=10;
